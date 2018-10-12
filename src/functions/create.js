7dfb7ba68b8bd906e6dccad77554be74d4b9ba43
cuongtv <truongcuong90@gmail.com>
2018-10-12 09:11:48 +0700

    - using env

--- src/functions/create.js
+++ src/functions/create.js
@@ -2,6 +2,7 @@ import serializeError from 'serialize-error'
 
 import formatParams from './format-params'
 import mediaMapping from 'mapping/media'
+import config from 'infrastructure/config'
 import media from 'services/media'
 import elasticSearch from 'services/elastic-search'
 
@@ -17,7 +18,7 @@ export default async (event) => {
             params: mediaMapping
           })
           const s3Object = await media.head({
-            bucket,
+            bucket: config.aws.s3.bucket,
             key: file.s3.object.key
           })
           const params = formatParams({ s3Object, key: file.s3.object.key })
