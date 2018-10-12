db5c754c4a5ced4a4499c94bbff67166c90497a6
cuongtv <truongcuong90@gmail.com>
2018-10-12 09:19:51 +0700

    - remove try catch in services

--- src/services/media.js
+++ src/services/media.js
@@ -4,29 +4,15 @@ import s3 from 'infrastructure/s3'
 
 export default {
   async head({ bucket, key }) {
-    try {
-      return await s3.headObject({
-        Bucket: bucket,
-        Key: key
-      }).promise()
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: serializeError(error)
-      }
-    }
+    return await s3.headObject({
+      Bucket: bucket,
+      Key: key
+    }).promise()
   },
   async list({ bucket, params }) {
-    try {
-      return await s3.listObjectsV2({
-        Bucket: bucket,
-        ...params
-      }).promise()
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: serializeError(error)
-      }
-    }
+    return await s3.listObjectsV2({
+      Bucket: bucket,
+      ...params
+    }).promise()
   }
 }
