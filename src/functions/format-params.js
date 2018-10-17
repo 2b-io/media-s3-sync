29eecef10f3ee20272542abee0b31e5c617bd79f
Luc Hoang Long <i.me.mine@luchoanglong.com>
2018-10-17 12:03:15 +0700

    review code & fix

--- src/functions/format-params.js
+++ src/functions/format-params.js
@@ -1,16 +1,19 @@
-const formatParams = ({ s3Object, key }) =>{
-  // key struct =   `version/identifier/hashedURL/presetHash/mode/...
+const formatParams = ({ s3Object, key }) => {
+  // key origin = `version/identifier/hashedURL`
+  // key target = `version/identifier/hashedURL/presetHash/...`
   const params = key.split('/')
   const identifier = params[1]
-  const preset = params.length > 2 ? params[3] : null
-  return({
-    lastModified: s3Object.LastModified,
-    contentLength: s3Object.ContentLength,
-    contentType: s3Object.ContentType,
+  const preset = params[3] || null
+  
+  return {
     key,
     identifier,
     preset,
+    lastModified: s3Object.LastModified,
+    contentLength: s3Object.ContentLength,
+    contentType: s3Object.ContentType,
     originUrl: s3Object.Metadata && s3Object.Metadata['origin-url'] || null
-  })
+  }
 }
+
 export default formatParams
