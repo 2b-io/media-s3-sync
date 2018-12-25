91e08d53039f22b2e7cceae8f6bca38023e3acff
cuongtv <truongcuong90@gmail.com>
2018-12-25 10:35:44 +0700

    - Remove function

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,2 @@
-export { default as clean } from './functions/clean'
 export { default as create } from './functions/create'
-export { default as migrate } from './functions/migrate'
 export { default as remove } from './functions/remove'
